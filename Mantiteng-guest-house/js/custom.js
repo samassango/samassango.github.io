da43730fd199f943d8f586ea98126d23d5ac0fe8
Sibusiso <samassango@gmail.com>
2021-09-12 09:14:40 +0200

    fixing the send email

--- Mantiteng-guest-house/js/custom.js
+++ Mantiteng-guest-house/js/custom.js
@@ -1,30 +1,30 @@
-$(function(){
+$(function () {
 
    'use strict';
 
    var isMobile = {
-      Android: function() {
+      Android: function () {
          return navigator.userAgent.match(/Android/i);
       },
-         BlackBerry: function() {
+      BlackBerry: function () {
          return navigator.userAgent.match(/BlackBerry/i);
       },
-         iOS: function() {
+      iOS: function () {
          return navigator.userAgent.match(/iPhone|iPad|iPod/i);
       },
-         Opera: function() {
+      Opera: function () {
          return navigator.userAgent.match(/Opera Mini/i);
       },
-         Windows: function() {
+      Windows: function () {
          return navigator.userAgent.match(/IEMobile/i);
       },
-         any: function() {
+      any: function () {
          return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
       }
    };
 
-	// Main Menu Superfish
-   var mainMenu = function() {
+   // Main Menu Superfish
+   var mainMenu = function () {
 
       $('#fh5co-primary-menu').superfish({
          delay: 0,
@@ -39,23 +39,23 @@ $(function(){
    };
 
    // Offcanvas and cloning of the main menu
-   var offcanvas = function() {
+   var offcanvas = function () {
 
       var $clone = $('#fh5co-menu-wrap').clone();
       $clone.attr({
-         'id' : 'offcanvas-menu'
+         'id': 'offcanvas-menu'
       });
       $clone.find('> ul').attr({
-         'class' : '',
-         'id' : ''
+         'class': '',
+         'id': ''
       });
 
       $('#fh5co-page').prepend($clone);
 
       // click the burger
-      $('.js-fh5co-nav-toggle').on('click', function(){
+      $('.js-fh5co-nav-toggle').on('click', function () {
 
-         if ( $('body').hasClass('fh5co-offcanvas') ) {
+         if ($('body').hasClass('fh5co-offcanvas')) {
             $('body').removeClass('fh5co-offcanvas');
             $(this).removeClass('active');
          } else {
@@ -68,75 +68,75 @@ $(function(){
 
       $('#offcanvas-menu').css('height', $(window).height());
 
-      $(window).resize(function(){
+      $(window).resize(function () {
          var w = $(window);
 
 
          $('#offcanvas-menu').css('height', w.height());
 
-         if ( w.width() > 769 ) {
-            if ( $('body').hasClass('fh5co-offcanvas') ) {
+         if (w.width() > 769) {
+            if ($('body').hasClass('fh5co-offcanvas')) {
                $('body').removeClass('fh5co-offcanvas');
             }
          }
 
-      });   
+      });
 
    }
-   
+
 
    // Click outside of the Mobile Menu
-   var mobileMenuOutsideClick = function() {
+   var mobileMenuOutsideClick = function () {
       $(document).click(function (e) {
-       var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
-       if (!container.is(e.target) && container.has(e.target).length === 0) {
-         if ( $('body').hasClass('fh5co-offcanvas') ) {
-            $('body').removeClass('fh5co-offcanvas');
+         var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+         if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($('body').hasClass('fh5co-offcanvas')) {
+               $('body').removeClass('fh5co-offcanvas');
+            }
          }
-       }
       });
    };
 
-   var counter = function() {
+   var counter = function () {
       $('.js-counter').countTo({
          formatter: function (value, options) {
-         return value.toFixed(options.decimals);
-       },
+            return value.toFixed(options.decimals);
+         },
       });
    };
 
-   var contentWayPoint = function() {
+   var contentWayPoint = function () {
       var i = 0;
-      $('.animate-box').waypoint( function( direction ) {
+      $('.animate-box').waypoint(function (direction) {
 
-         if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-            
+         if (direction === 'down' && !$(this.element).hasClass('animated')) {
+
             i++;
 
             $(this.element).addClass('item-animate');
-            setTimeout(function(){
+            setTimeout(function () {
 
-               $('body .animate-box.item-animate').each(function(k){
+               $('body .animate-box.item-animate').each(function (k) {
                   var el = $(this);
-                  setTimeout( function () {
+                  setTimeout(function () {
                      el.addClass('fadeInUp animated');
                      el.removeClass('item-animate');
-                  },  k * 200, 'easeInOutExpo' );
+                  }, k * 200, 'easeInOutExpo');
                });
-               
+
             }, 100);
-            
+
          }
 
-      } , { offset: '85%' } );
+      }, { offset: '85%' });
    };
 
 
-   var fullHeight = function() {
+   var fullHeight = function () {
 
-      if ( !isMobile.any() ) {
+      if (!isMobile.any()) {
          $('.js-fullheight').css('height', $(window).height() - $('#fh5co-header').height());
-         $(window).resize(function(){
+         $(window).resize(function () {
             $('.js-fullheight').css('height', $(window).height() - $('#fh5co-header').height());
          });
       }
@@ -148,57 +148,95 @@ $(function(){
 
    $('#date-start, #date-end').datepicker();
 
-   [].slice.call( document.querySelectorAll( 'select.cs-select' ) ).forEach( function(el) {  
+   [].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
       new SelectFx(el);
-   } );
+   });
 
+   var sendmail = function () {
 
-   
+      $('#submit-email').on('click', function () {
+         console.log('testing submit email');
+         var startDate = $('#date-start').val();
+         var endDate = $('#date-end').val();
+         if (startDate == "") {
+            alert('Please select state date')
+            return false
+         }
+
+         if (endDate == "") {
+            alert('Please select end date')
+            return false
+         }
+
+         var selected = $('#guest-houses option:selected').val();
+         $(this).attr('href', 'mailto:mantiteng@gmail.com?body= Hi Mantiteng Guest house. I want to place a reservation for a ' + selected + ' from ' + startDate + ' to ' + endDate + ' &subject=Booking request for ' + selected)
+      });
+   }
+   var sendMessage = function () {
+      $('#submit').on('click', function (e) {
+         var name = $('#user-name').val();
+         var email = $('#user-email').val();
+         var message = $('textarea#user-message').val();
+         if (name == "") {
+            alert('Please enter a name')
+            return false;
+         }
+         if (email == "") {
+            alert('Please enter a email')
+            return false;
+         }
+         if (message == "") {
+            alert('Please enter a message')
+            return false;
+         }
+         location.href = 'mailto:mantiteng@gmail.com?body= Hi Mantiteng, My name is ' + name + '. My email address is ' + email + '. ' + message + '&subject=Guest House Enquiries';
+      })
+   }
    // Tabs
 
-   var tabs = function() {
+   var tabs = function () {
       $('#hotel-facilities').css('height', $('.tab-content.active').height() + 600);
 
-      $(window).resize(function(){
+      $(window).resize(function () {
          $('#hotel-facilities').css('height', $('.tab-content.active').height() + 600);
       });
 
-      $('.tabs-nav > a').on('click', function(e){
-         
+      $('.tabs-nav > a').on('click', function (e) {
+
          var tab = $(this).data('tab');
 
          $('.tabs-nav > a').removeClass('active');
          $(this).addClass('active');
 
          $('.tab-content').removeClass('active show');
-         
-         setTimeout(function(){
-            $('.tab-content[data-tab-content="'+tab+'"]').addClass('active');
+
+         setTimeout(function () {
+            $('.tab-content[data-tab-content="' + tab + '"]').addClass('active');
             $('#hotel-facilities').css('height', $('.tab-content.active').height() + 600);
          }, 200);
-         setTimeout(function(){
-            $('.tab-content[data-tab-content="'+tab+'"]').addClass('show');
+         setTimeout(function () {
+            $('.tab-content[data-tab-content="' + tab + '"]').addClass('show');
          }, 400);
-         
+
 
          e.preventDefault();
       });
    };
 
-   var sliderMain = function() {
-      
+   var sliderMain = function () {
+
       $('#fh5co-hero .flexslider').flexslider({
          animation: "fade",
          slideshowSpeed: 5000,
          directionNav: true,
-         start: function(){
-            setTimeout(function(){
+         start: function () {
+            setTimeout(function () {
                $('.slider-text').removeClass('animated fadeInUp');
                $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
             }, 500);
          },
-         before: function(){
-            setTimeout(function(){
+         before: function () {
+            setTimeout(function () {
                $('.slider-text').removeClass('animated fadeInUp');
                $('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
             }, 500);
@@ -206,20 +244,20 @@ $(function(){
 
       });
 
-      $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());  
-      $(window).resize(function(){
-         $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());  
+      $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());
+      $(window).resize(function () {
+         $('#fh5co-hero .flexslider .slides > li').css('height', $(window).height());
       });
 
    };
 
    // Parallax
-   var parallax = function() {
+   var parallax = function () {
       $(window).stellar();
    };
 
 
-   $(function(){
+   $(function () {
       sliderMain();
       tabs();
       mainMenu();
@@ -229,8 +267,9 @@ $(function(){
       parallax();
       fullHeight();
       counter();
+      sendmail();
+      sendMessage();
    });
 
 });
-	
-	
+
